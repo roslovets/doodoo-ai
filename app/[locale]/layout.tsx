908c6db5f24d6eeb2411b53b370206be2e379a91
Pavel Roslovets <p.v.roslovets@gmail.com>
2024-05-05 23:30:59 +0200

    feat: finish rebranding

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "Doodoo AI"
+const APP_DEFAULT_TITLE = "Doodoo AI"
+const APP_TITLE_TEMPLATE = "%s - Doodoo AI"
+const APP_DESCRIPTION = "Doodoo AI PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
